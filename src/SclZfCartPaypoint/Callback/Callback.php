bc2c91afddcd83a93141db3b8d69d4023f26b007
Tom Oram <tom@scl.co.uk>
2013-06-10 16:04:22 +0100

    Callback class now complete

--- src/SclZfCartPaypoint/Callback/Callback.php
+++ src/SclZfCartPaypoint/Callback/Callback.php
@@ -2,6 +2,7 @@
 
 namespace SclZfCartPaypoint\Callback;
 
+use SclZfCartPaypoint\Exception\DomainException;
 /**
  * Packages up the callback data nicely.
  *
@@ -9,108 +10,6 @@ namespace SclZfCartPaypoint\Callback;
  */
 class Callback
 {
-    /*
-     * Status codes.
-     */
-
-    /**
-     * Transaction authorised by bank. auth_code available as bank reference.
-     */
-    const STATUS_AUTHORISED = 'A';
-
-    /**
-     *  Transaction not authorised. Failure message text available to merchant.
-     */
-    const STATUS_NOT_AUTHORISED = 'N';
-
-    /**
-     * Communication problem. Trying again later may well work.
-     */
-    const STATUS_COMMUNICATION_ERROR = 'C';
-
-    /**
-     * The PayPoint.net system has detected a fraud condition and rejected the
-     * transaction. The message field will contain more details.
-     */
-    const STATUS_FRAUD = 'F';
-
-    /**
-     * Pre-bank checks. Amount not supplied or invalid.
-     */
-    const STATUS_PRE_NO_AMOUNT = 'P:A';
-
-    /**
-     * Pre-bank checks. Not all mandatory parameters supplied.
-     */
-    const STATUS_PRE_MISSING_PARAMS = 'P:X';
-
-    /**
-     * Pre-bank checks. Same payment presented twice.
-     */
-    const STATUS_PRE_DUPLICATE_PAYMENT = 'P:P';
-
-    /**
-     * Pre-bank checks. Start date invalid.
-     */
-    const STATUS_PRE_START_DATE = 'P:S';
-
-    /**
-     * Pre-bank checks. Expiry date invalid.
-     */
-    const STATUS_PRE_EXPIRY_DATE = 'P:E';
-
-    /**
-     * Pre-bank checks. Issue number invalid.
-     */
-    const STATUS_PRE_ISSUE_NO = 'P:I';
-
-    /**
-     * Pre-bank checks. Card number fails LUHN check
-     * (the card number is wrong).
-     */
-    const STATUS_PRE_LUHN = 'P:C';
-
-    /**
-     * Pre-bank checks. Card type invalid - i.e. does not match card number prefix.
-     */
-    const STATUS_TYPE = 'P:T';
-
-    /**
-     * Pre-bank checks. Customer name not supplied.
-     */
-    const STATUS_PRE_NAME = 'P:N';
-
-    /**
-     * Pre-bank checks. Merchant does not exist or not registered yet.
-     */
-    const STATUS_PRE_BAD_MERCHANT = 'P:M';
-
-    /**
-     * Pre-bank checks. Merchant account for card type does not exist.
-     */
-    const STATUS_PRE_CARD_TYPE = 'P:B';
-
-    /**
-     * Pre-bank checks. Merchant account for this currency does not exist.
-     */
-    const STATUS_CURRENCY = 'P:D';
-
-    /**
-     * Pre-bank checks. CV2 security code mandatory and not supplied / invalid.
-     */
-    const STATUS_CV2 = 'P:V';
-
-    /**
-     * Pre-bank checks. Transaction timed out awaiting a virtual circuit.
-     * Merchant may not have enough virtual circuits for the volume of business.
-     */
-    const STATUS_TIMEOUT = 'P:R';
-
-    /**
-     * Pre-bank checks. No MD5 hash / token key set up against account.
-     */
-    const STATUS_TOKEN = 'P:#';
-
     /*
      * Response data keys
      */
@@ -154,56 +53,341 @@ class Callback
      */
     protected $cv2avs;
 
+    /**
+     * A message is returned if the transaction is not authorized.
+     *
+     * @var string
+     */
     protected $message;
 
+    /**
+     * The response code from the back is the transaction failed.
+     *
+     * @var int
+     */
     protected $responseCode;
 
+    /**
+     * The amount authorized by th bank.
+     *
+     * @var mixed
+     */
     protected $amount;
 
+    /**
+     * The status code returned from Paypoint.
+     *
+     * @var StatusCode
+     */
     protected $code;
 
+    /**
+     * Was the API running in test mode.
+     *
+     * @var bool
+     */
     protected $testStatus;
 
+    /**
+     * The verification hash.
+     *
+     * @var string
+     */
     protected $hash;
 
     /**
-     * Possible values for the status code.
+     * setValueFromArray
+     *
+     * @param  array  $values
+     * @param  string $key
+     * @param  string $method
+     * @return void
      */
-    protected static $statusValues = array(
-        self::STATUS_AUTHORISED,
-        self::STATUS_NOT_AUTHORISED,
-        self::STATUS_COMMUNICATION_ERROR,
-        self::STATUS_FRAUD,
-        self::STATUS_PRE_NO_AMOUNT,
-        self::STATUS_PRE_MISSING_PARAMS,
-        self::STATUS_PRE_DUPLICATE_PAYMENT,
-        self::STATUS_PRE_START_DATE,
-        self::STATUS_PRE_EXPIRY_DATE,
-        self::STATUS_PRE_ISSUE_NO,
-        self::STATUS_PRE_LUHN,
-        self::STATUS_TYPE,
-        self::STATUS_PRE_NAME,
-        self::STATUS_PRE_BAD_MERCHANT,
-        self::STATUS_PRE_CARD_TYPE,
-        self::STATUS_CURRENCY,
-        self::STATUS_CV2,
-        self::STATUS_TIMEOUT,
-        self::STATUS_TOKEN,
-    );
+    protected function setValueFromArray(array $values, $key, $method)
+    {
+        if (isset($values[$key])) {
+            $this->$method($values[$key]);
+        }
+    }
 
     /**
-     * Possible callback key values.
+     * setBoolStringValue
+     *
+     * @param  array  $values
+     * @param  string $key
+     * @param  string $method
+     * @return void
+     * @throws DomainException When value is not "true" or "false".
      */
-    protected static $keyValues = array(
-        self::KEY_TRANSACTION_ID,
-        self::KEY_VALID,
-        self::KEY_AUTH_CODE,
-        self::KEY_CV2AVS,
-        self::KEY_MESSAGE,
-        self::KEY_RESPONSE_CODE,
-        self::KEY_AMOUNT,
-        self::KEY_CODE,
-        self::KEY_TEST_STATUS,
-        self::KEY_HASH,
-    );
+    protected function setBoolStringValue(array $values, $key, $method)
+    {
+        if (!isset($values[$key])) {
+            return;
+        }
+
+        if ('true' === $values[$key]) {
+            $this->$method(true);
+            return;
+        }
+
+        if ('false' === $values[$key]) {
+            $this->$method(false);
+            return;
+        }
+
+        throw new DomainException(
+            sprintf(
+                '$values[%s] must be "true" or "false"; got "%s"',
+                $key,
+                $values[$key]
+            )
+        );
+    }
+
+    /**
+     * Set all the values of the callback object.
+     *
+     * @param  array $values
+     * @return self
+     */
+    public function setCallbackValues(array $values)
+    {
+        $this->setValueFromArray($values, self::KEY_TRANSACTION_ID, 'setTransactionId');
+        $this->setValueFromArray($values, self::KEY_AUTH_CODE, 'setAuthCode');
+        $this->setValueFromArray($values, self::KEY_MESSAGE, 'setMessage');
+        $this->setValueFromArray($values, self::KEY_AMOUNT, 'setAmount');
+        $this->setValueFromArray($values, self::KEY_RESPONSE_CODE, 'setResponseCode');
+        $this->setValueFromArray($values, self::KEY_HASH, 'setHash');
+
+        $this->setBoolStringValue($values, self::KEY_VALID, 'setValid');
+        $this->setBoolStringValue($values, self::KEY_TEST_STATUS, 'setTestStatus');
+
+        if (isset($values[self::KEY_CV2AVS])) {
+            $this->setCv2avs(new CV2AVSResponse($values[self::KEY_CV2AVS]));
+        }
+
+        if (isset($values[self::KEY_CODE])) {
+            $this->setCode(new StatusCode($values[self::KEY_CODE]));
+        }
+    }
+
+    /**
+     * Gets the value of transactionId
+     *
+     * @return string
+     */
+    public function getTransactionId()
+    {
+        return $this->transactionId;
+    }
+
+    /**
+     * Sets the value of transactionId
+     *
+     * @param  string $transactionId
+     * @return self
+     */
+    public function setTransactionId($transactionId)
+    {
+        $this->transactionId = (string) $transactionId;
+        return $this;
+    }
+
+    /**
+     * Gets the value of valid
+     *
+     * @return bool
+     */
+    public function getValid()
+    {
+        return $this->valid;
+    }
+
+    /**
+     * Sets the value of valid
+     *
+     * @param  bool $valid
+     * @return self
+     */
+    public function setValid($valid)
+    {
+        $this->valid = (bool) $valid;
+        return $this;
+    }
+
+    /**
+     * Gets the value of authCode
+     *
+     * @return int
+     */
+    public function getAuthCode()
+    {
+        return $this->authCode;
+    }
+
+    /**
+     * Sets the value of authCode
+     *
+     * @param  int $authCode
+     * @return self
+     */
+    public function setAuthCode($authCode)
+    {
+        $this->authCode = (int) $authCode;
+        return $this;
+    }
+
+    /**
+     * Gets the value of cv2avs
+     *
+     * @return CV2AVSResponse
+     */
+    public function getCv2avs()
+    {
+        return $this->cv2avs;
+    }
+
+    /**
+     * Sets the value of cv2avs
+     *
+     * @param  CV2AVSResponse $cv2avs
+     * @return self
+     */
+    public function setCv2avs(CV2AVSResponse $cv2avs)
+    {
+        $this->cv2avs = $cv2avs;
+        return $this;
+    }
+
+    /**
+     * Gets the value of message
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Sets the value of message
+     *
+     * @param  string $message
+     * @return self
+     */
+    public function setMessage($message)
+    {
+        $this->message = (string) $message;
+        return $this;
+    }
+
+    /**
+     * Gets the value of responseCode
+     *
+     * @return int
+     */
+    public function getResponseCode()
+    {
+        return $this->responseCode;
+    }
+
+    /**
+     * Sets the value of responseCode
+     *
+     * @param  int $responseCode
+     * @return self
+     */
+    public function setResponseCode($responseCode)
+    {
+        $this->responseCode = (int) $responseCode;
+        return $this;
+    }
+
+    /**
+     * Gets the value of amount
+     *
+     * @return float
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * Sets the value of amount
+     *
+     * @param  float $amount
+     * @return self
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = (float) $amount;
+        return $this;
+    }
+
+    /**
+     * Gets the value of code
+     *
+     * @return StatusCode
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Sets the value of code
+     *
+     * @param  StatusCode $code
+     * @return self
+     */
+    public function setCode(StatusCode $code)
+    {
+        $this->code = $code;
+        return $this;
+    }
+
+    /**
+     * Gets the value of testStatus
+     *
+     * @return bool
+     */
+    public function getTestStatus()
+    {
+        return $this->testStatus;
+    }
+
+    /**
+     * Sets the value of testStatus
+     *
+     * @param  bool $testStatus
+     * @return self
+     */
+    public function setTestStatus($testStatus)
+    {
+        $this->testStatus = (bool) $testStatus;
+        return $this;
+    }
+
+    /**
+     * Gets the value of hash
+     *
+     * @return string
+     */
+    public function getHash()
+    {
+        return $this->hash;
+    }
+
+    /**
+     * Sets the value of hash
+     *
+     * @param  string $hash
+     * @return self
+     */
+    public function setHash($hash)
+    {
+        $this->hash = (string) $hash;
+        return $this;
+    }
 }
--- tests/SclZfCartPaypointTests/Callback/CallbackTest.php
+++ tests/SclZfCartPaypointTests/Callback/CallbackTest.php
@@ -3,6 +3,8 @@
 namespace SclZfCartPaypointTests\Callback;
 
 use SclZfCartPaypoint\Callback\Callback;
+use SclZfCartPaypoint\Callback\StatusCode;
+use SclZfCartPaypoint\Callback\CV2AVSResponse;
 
 /**
  * Unit tests for {@see Callback}.
@@ -18,9 +20,197 @@ class CallbackTest extends \PHPUnit_Framework_TestCase
      */
     protected $callback;
 
-    public function testInitialisation()
+    /**
+     * Prepare the instance to be tested.
+     *
+     * @return void
+     */
+    protected function setUp()
     {
-        $this->callback = new Callback();
+        $this->callback = new Callback;
+    }
+
+    /**
+     * testGettersAndSetters
+     *
+     * @dataProvider getSetDataProvider
+     * @covers SclZfCartPaypoint\Callback\Callback::setTransactionId
+     * @covers SclZfCartPaypoint\Callback\Callback::getTransactionId
+     * @covers SclZfCartPaypoint\Callback\Callback::setValid
+     * @covers SclZfCartPaypoint\Callback\Callback::getValid
+     * @covers SclZfCartPaypoint\Callback\Callback::setAuthCode
+     * @covers SclZfCartPaypoint\Callback\Callback::getAuthCode
+     * @covers SclZfCartPaypoint\Callback\Callback::setCv2avs
+     * @covers SclZfCartPaypoint\Callback\Callback::getCv2avs
+     * @covers SclZfCartPaypoint\Callback\Callback::setMessage
+     * @covers SclZfCartPaypoint\Callback\Callback::getMessage
+     * @covers SclZfCartPaypoint\Callback\Callback::setResponseCode
+     * @covers SclZfCartPaypoint\Callback\Callback::getResponseCode
+     * @covers SclZfCartPaypoint\Callback\Callback::setAmount
+     * @covers SclZfCartPaypoint\Callback\Callback::getAmount
+     * @covers SclZfCartPaypoint\Callback\Callback::setCode
+     * @covers SclZfCartPaypoint\Callback\Callback::getCode
+     * @covers SclZfCartPaypoint\Callback\Callback::setTestStatus
+     * @covers SclZfCartPaypoint\Callback\Callback::getTestStatus
+     * @covers SclZfCartPaypoint\Callback\Callback::setHash
+     * @covers SclZfCartPaypoint\Callback\Callback::getHash
+     *
+     * @param  string $name  The name of the get/set function to test.
+     * @param  string $value The value to test with.
+     * @return void
+     */
+    public function testGettersAndSetters($name, $value)
+    {
+        $getter = 'get' . $name;
+        $setter = 'set' . $name;
+
+        $result = $this->callback->$setter($value);
+
+        $this->assertEquals(
+            $this->callback,
+            $result,
+            $setter . ' is not fluid.'
+        );
+
+        $this->assertEquals(
+            $value,
+            $this->callback->$getter(),
+            $getter . ' did not return the correct value.'
+        );
+    }
+
+    /**
+     * Provides the data to test the getters and setters.
+     *
+     * @return array
+     */
+    public function getSetDataProvider()
+    {
+        return array(
+            array('transactionId', 'TRANSX'),
+            array('valid', true),
+            array('authCode', 8),
+            array('cv2avs', new CV2AVSResponse()),
+            array('message', 'BLAH'),
+            array('responseCode', 12),
+            array('amount', 5.99),
+            array('code', new StatusCode()),
+            array('testStatus', false),
+            array('hash', 'abcxyz'),
+        );
+    }
+
+    /**
+     * Test setting the values of the callback object.
+     *
+     * @covers SclZfCartPaypoint\Callback\Callback::setCallbackValues
+     * @covers SclZfCartPaypoint\Callback\Callback::setValueFromArray
+     * @covers SclZfCartPaypoint\Callback\Callback::setBoolStringValue
+     *
+     * @return void
+     */
+    public function testSetCallbackValues()
+    {
+        $values = array(
+            'trans_id'    => 'TR000',
+            'valid'       => 'true',
+            'auth_code'   => '99',
+            'cv2avs'      => 'ALL MATCH',
+            'message'     => 'SUCCESS',
+            'resp_code'   => '21',
+            'amount'      => '4.88',
+            'code'        => 'A',
+            'test_status' => 'false',
+            'hash'        => 'LE_HASH',
+        );
+
+        $this->callback->setCallbackValues($values);
+
+        $this->assertEquals(
+            $values['trans_id'],
+            $this->callback->getTransactionId(),
+            'transactionId value is incorrect.'
+        );
+
+        $this->assertTrue(
+            $this->callback->getValid(),
+            'valid value is incorrect.'
+        );
+
+        $this->assertEquals(
+            99,
+            $this->callback->getAuthCode(),
+            'authCode value is incorrect.'
+        );
+
+        $this->assertTrue(
+            $this->callback->getCv2avs()->allMatch(),
+            'cv2avs value is incorrect.'
+        );
+
+        $this->assertEquals(
+            $values['message'],
+            $this->callback->getMessage(),
+            'message value is incorrect.'
+        );
+
+        $this->assertEquals(
+            $values['resp_code'],
+            $this->callback->getResponseCode(),
+            'responseCode value is incorrect.'
+        );
+
+        $this->assertEquals(
+            $values['amount'],
+            $this->callback->getAmount(),
+            'amount value is incorrect.'
+        );
+
+        $this->assertEquals(
+            $values['code'],
+            $this->callback->getCode()->value(),
+            'code value is incorrect.'
+        );
+
+        $this->assertFalse(
+            $this->callback->getTestStatus(),
+            'testStatus value is incorrect.'
+        );
+
+        $this->assertEquals(
+            $values['hash'],
+            $this->callback->getHash(),
+            'hash value is incorrect.'
+        );
+    }
+
+    /**
+     * Test that if a boolean value is not set then the value remains unset.
+     *
+     * @depends testSetCallbackValues
+     * @covers SclZfCartPaypoint\Callback\Callback::setBoolStringValue
+     *
+     * @return void
+     */
+    public function testSetCallbackValuesWithNoBoolValue()
+    {
+        $this->callback->setCallbackValues(array());
+
+        $this->assertNull($this->callback->getTestStatus());
+    }
+
+    /**
+     * Test that an exception is thrown if a boolean value contains a string
+     * which is not "true" or "false".
+     *
+     * @depends testSetCallbackValues
+     * @covers SclZfCartPaypoint\Callback\Callback::setBoolStringValue
+     * @expectedException SclZfCartPaypoint\Exception\DomainException
+     *
+     * @return void
+     */
+    public function setSetCallbackValuesWithBadBoolString()
+    {
+        $this->callback->getCallbackValues(array('test_status' => 'y'));
     }
 }
-
